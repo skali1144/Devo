f79df660e85ead9174c52e40be77f84674c19ff8
skali1144 <115889177+skali1144@users.noreply.github.com>
2023-09-01 15:54:00 +0400

    Update devo.sh

--- devo.sh
+++ devo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ECHO"Running devo.sh..."
 ECHO"if it asks for your password write the password of your login system."
-sleep(5)
 sudo apt update
 sudo apt upgrade
 sudo apt install kali-linux-everything
