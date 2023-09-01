abc786c79707f71695acd4ed14076f9a8ced790b
skali1144 <115889177+skali1144@users.noreply.github.com>
2023-09-01 16:01:53 +0400

    Update devo.sh

--- devo.sh
+++ devo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ECHO"Running devo.sh..."
-ECHO" if it asks for your password write the password of your login system."
+"Running devo.sh..."
+" if it asks for your password write the password of your login system."
 sleep 5
 sudo apt update
 sudo apt upgrade
